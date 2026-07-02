abd826cd322c8ec1d28cf79d38e2201c9d1aa1d7
Tiago1710 <tiagocorvalan4@gmail.com>
2026-07-01 21:14:53 -0300

    Add files via upload

---
+++ TemaContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+
+export const TemaContext = createContext();
+
+export function TemaProvider({ children }) {
+  const [tema, setTema] = useState('claro');
+
+  const alternarTema = () => {
+    setTema(tema === 'claro' ? 'oscuro' : 'claro');
+  };
+
+  return (
+    <TemaContext.Provider value={{ tema, alternarTema }}>
+      {children}
+    </TemaContext.Provider>
+  );
+}
